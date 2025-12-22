0ee53bc2e286e5be34a0129c2b9ea8103133531e
fajar-dev <fajarrivaldi2015@gmail.com>
2025-12-22 10:52:22 +0700

    init: bootsraping langchain

---
+++ src/config/database.ts
@@ -0,0 +1,28 @@
+import { PoolConfig } from "pg";
+import { PGVectorStoreArgs } from "@langchain/community/vectorstores/pgvector";
+import {
+    DB_HOST,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_PORT,
+    DB_USER,
+} from './config'
+
+const poolConfig: PoolConfig = {
+    host: DB_HOST,
+    port: parseInt(DB_PORT || "5432"),
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+};
+
+export const vectorStoreConfig: PGVectorStoreArgs = {
+    postgresConnectionOptions: poolConfig,
+    tableName: "documents",
+    columns: {
+        idColumnName: "id",
+        vectorColumnName: "vector",
+        contentColumnName: "content",
+        metadataColumnName: "metadata",
+    },
+};
